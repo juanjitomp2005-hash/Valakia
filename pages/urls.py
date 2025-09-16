4fafeb335a889c400efc5eb64fad2fc7cda9bee8
juanjitomp2005-hash <juanjitomp2005@gmail.com>
2025-09-16 14:06:21 -0500

    nuevo
    carrito, barra de busqueda y sistema de pago (en proceso)

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView, register
 from django.contrib.auth import views as auth_views 
 from django.contrib import admin
+from .views import cart_view, add_to_cart, remove_from_cart
+from django.urls import path
+from . import views
 
 urlpatterns = [
  path('', HomePageView.as_view(), name='home'),
@@ -12,5 +15,13 @@ urlpatterns = [
  path('login/', auth_views.LoginView.as_view(template_name='pages/login.html'), name='login'),
  path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
  path("register/", register, name="register"),
+ path("cart/", cart_view, name="cart"),
+ path("cart/add/<int:product_id>/", add_to_cart, name="add_to_cart"),
+ path("cart/remove/<int:product_id>/", remove_from_cart, name="remove_from_cart"),
+ path("checkout/", views.checkout_view, name="checkout"),
+ path("success/", views.success_view, name="success"),
+ path("cancel/", views.cancel_view, name="cancel"),
+ path("success/", views.success_view, name="success"),
+ path("cancel/", views.cancel_view, name="cancel"),
 
 ]
